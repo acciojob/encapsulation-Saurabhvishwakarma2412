4aaef1a0561fabb8ad7d59dbf263dfcd3706350c
saurabh <shivajeetv@gmail.com>
2023-09-12 04:36:47 +0530

    learned encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public  static void main(String[] args) {
+      RWOnly obj=new RWOnly();
+     // obj.name="Saurabh";
+    //  System.out.println(obj.name);
+      obj.setter("saurabh");
+      String ans=obj.getter();
+      System.out.println(ans);
+  }
 }
